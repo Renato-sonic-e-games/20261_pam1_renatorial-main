7d6fbd3b714f7864a566de643c8d721a28466660
renato-sonic-e-games <rennerutame009@gmail.com>
2026-05-12 21:56:47 -0300

    atv1

--- lib/main.dart
+++ lib/main.dart
@@ -34,9 +34,30 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Etec Adolpho Berezin - PAM1'),
-          backgroundColor: Colors.redAccent,
+      // header do aplicativo
+      appBar: AppBar(  // construtor da classe AppBar
+        title: const Text('Etec Adolpho Berezin - PAM1'), // titulo do header
+          backgroundColor: Colors.redAccent, // cor de fundo do header
+      ),
+      //menu lateral esquerdo
+      drawer: Drawer( // construtor da classe Drawer
+      child: Text("Menu esquerdo"), // conteudo interno
+      backgroundColor: Colors.redAccent, // cor de fundo
+      ),
+      //menu lateral direito
+      endDrawer: Drawer( // construtor da classe endDrawer
+      child: Text("Menu direito"), // conteudo interno
+      backgroundColor: Colors.redAccent, // cor de fundo
+      ),
+      // footer
+      bottomNavigationBar: BottomAppBar( // construtor da classe BottomAppBar
+        color: Colors.redAccent, // cor de fundo
+        child: Text("Desenvolvimento Mobile com Flutter") // conteudo interno
+      ),
+      //botão flutuante
+      floatingActionButton: FloatingActionButton( // construtor da classe FloatingActionButton
+        onPressed: () {}, // ação do botão
+        child: const Icon(Icons.add), // ícone do botão
       ),
       body: const Center(
         child: Text(
